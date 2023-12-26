d923e316ed6c4a197f6ce9ffa5de5ed575cd8288
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2023-12-26 22:00:27 +0530

    added webpack config files

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("I don't trust stairs, they are always up to something")})();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules:[{
+        test:/\.scss$/,
+        use:[
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
+    }]
+  },
+  plugins:[
+    new htmlWebpackPlugin({
+        title:"Webpack App",
+        filename:"index.html"
+    })
+  ]
+};
