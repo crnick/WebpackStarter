c6ae90ca9fb32bab599b949c183b35388ed9a4c9
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2023-12-27 21:57:58 +0530

    added dev server and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin")
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,21 +7,46 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    assetModuleFilename:"[name][ext]" // so name stays the same
   },
+  devtool: "source-map", // get js map file
+  devServer: {
+    //development server
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true, //opens browser automatically
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
   module: {
-    rules:[{
-        test:/\.scss$/,
-        use:[
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
-    }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exlude: /node_modules/,
+        use: ["babel-loader"],
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // i is if it is case sensitive.
+        type: "asset/resource",
+      },
+    ],
   },
-  plugins:[
+  plugins: [
     new htmlWebpackPlugin({
-        title:"Webpack App",
-        filename:"index.html"
-    })
-  ]
+      title: "Webpack App",
+      filename: "index.html",
+      template: "/src/template.html",
+    }),
+  ],
 };
